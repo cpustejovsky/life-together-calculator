fba1a671327b2c02ffdcd42017c38b14685b03ba
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-13 17:35:27 -0400

    updating state from form and conditional rendering of display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,48 +3,149 @@ import React, { Component } from "react";
 export default class Display extends Component {
   render() {
     const {
-      name,
-      sigOthName,
-      bday,
-      sigOthBday,
-      dateMet,
-      dateDating,
-      dateMarried
+      yourName,
+      soName,
+      yourBday,
+      soBday,
+      meetDate,
+      datingDate,
+      marriedDate
     } = this.props.data;
-    return (
-      <div>
-        <h1>Display</h1>
-        <ul>
-          <li>
-            <strong>Name: </strong>
-            {name}
-          </li>
-          <li>
-            <strong>Significant Other's Name: </strong>
-            {sigOthName}
-          </li>
-          <li>
-            <strong>Birthday: </strong>
-            {bday.day} {bday.month}, {bday.year}{" "}
-          </li>
-          <li>
-            <strong>Significant Other's Birthday: </strong>
-            {sigOthBday.day} {sigOthBday.month}, {sigOthBday.year}{" "}
-          </li>
-          <li>
-            <strong>Date You Met: </strong>
-            {dateMet.day} {dateMet.month}, {dateMet.year}{" "}
-          </li>
-          <li>
-            <strong>Date You Started Dating: </strong>
-            {dateDating.day} {dateDating.month}, {dateDating.year}{" "}
-          </li>
-          <li>
-            <strong>Date You Were Married: </strong>
-            {dateMarried.day} {dateMarried.month}, {dateMarried.year}{" "}
-          </li>
-        </ul>
-      </div>
-    );
+    if (
+      yourName === null ||
+      soName === null ||
+      yourBday === null ||
+      soBday === null ||
+      meetDate === null
+    ) {
+      return (
+        <h1>
+          Please provide at least two names, two birthdays, and a meeting day.
+        </h1>
+      );
+    }
+    if (
+      yourName !== null &&
+      soName !== null &&
+      yourBday !== null &&
+      soBday !== null &&
+      meetDate !== null &&
+      datingDate === null &&
+      marriedDate === null
+    ) {
+      return (
+        <div>
+          <h1>Display</h1>
+          <ul>
+            <li>
+              <strong>Name: </strong>
+              {yourName}
+            </li>
+            <li>
+              <strong>Significant Other's Name: </strong>
+              {soName}
+            </li>
+            <li>
+              <strong>Birthday: </strong>
+              {yourBday}{" "}
+            </li>
+            <li>
+              <strong>Significant Other's Birthday: </strong>
+              {soBday}{" "}
+            </li>
+            <li>
+              <strong>Date You Met: </strong>
+              {meetDate}{" "}
+            </li>
+          </ul>
+        </div>
+      );
+    }
+    if (
+      yourName !== null &&
+      soName !== null &&
+      yourBday !== null &&
+      soBday !== null &&
+      meetDate !== null &&
+      datingDate !== null &&
+      marriedDate === null
+    ) {
+      return (
+        <div>
+          <h1>Display</h1>
+          <ul>
+            <li>
+              <strong>Name: </strong>
+              {yourName}
+            </li>
+            <li>
+              <strong>Significant Other's Name: </strong>
+              {soName}
+            </li>
+            <li>
+              <strong>Birthday: </strong>
+              {yourBday}{" "}
+            </li>
+            <li>
+              <strong>Significant Other's Birthday: </strong>
+              {soBday}{" "}
+            </li>
+            <li>
+              <strong>Date You Met: </strong>
+              {meetDate}{" "}
+            </li>
+            <li>
+              <strong>Date You Started Dating: </strong>
+              {datingDate}{" "}
+            </li>
+          </ul>
+        </div>
+      );
+    }
+    if (
+      yourName !== null &&
+      soName !== null &&
+      yourBday !== null &&
+      soBday !== null &&
+      meetDate !== null &&
+      datingDate !== null &&
+      marriedDate !== null
+    ) {
+      return (
+        <div>
+          <h1>Display</h1>
+          <ul>
+            <li>
+              <strong>Name: </strong>
+              {yourName}
+            </li>
+            <li>
+              <strong>Significant Other's Name: </strong>
+              {soName}
+            </li>
+            <li>
+              <strong>Birthday: </strong>
+              {yourBday}{" "}
+            </li>
+            <li>
+              <strong>Significant Other's Birthday: </strong>
+              {soBday}{" "}
+            </li>
+            <li>
+              <strong>Date You Met: </strong>
+              {meetDate}{" "}
+            </li>
+            <li>
+              <strong>Date You Started Dating: </strong>
+              {datingDate}{" "}
+            </li>
+            <li>
+              <strong>Date You were Married: </strong>
+              {marriedDate}{" "}
+            </li>
+          </ul>
+        </div>
+      );
+    }
   }
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,87 +1,128 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
-  state = {
-    term: ""
+  data = {
+    yourName: null,
+    soName: null,
+    yourBday: null,
+    soBday: null,
+    meetDate: null,
+    datingDate: null,
+    marriedDate: null
   };
 
   onFormSubmit(e) {
     e.preventDefault();
-    console.log(e)
   }
-      
-  
+
+  onInputChange(e) {
+    switch (e.target.name) {
+      case "yourName":
+        this.data.yourName = e.target.value;
+        break;
+      case "soName":
+        this.data.soName = e.target.value;
+        break;
+      case "yourBday":
+        this.data.yourBday = e.target.value;
+        break;
+      case "soBday":
+        this.data.soBday = e.target.value;
+        break;
+      case "meetDate":
+        this.data.meetDate = e.target.value;
+        break;
+      case "datingDate":
+        this.data.datingDate = e.target.value;
+        break;
+      case "marriedDate":
+        this.data.marriedDate = e.target.value;
+        break;
+      default:
+        break;
+    }
+  }
+  handleDataChange = data => {
+    data = this.data;
+    console.log(data);
+    this.props.onUpdate(data);
+  };
   render() {
-    
     return (
       <div>
         <form onSubmit={e => this.onFormSubmit(e)}>
           <div className="form-group">
             <p>Your Name</p>
             <input
+              onChange={e => this.onInputChange(e)}
               className="form-control"
               type="text"
-              name="lifeTogether[yourName]"
+              name="yourName"
               placeholder="Aragorn"
-              required
             />
           </div>
           <div className="form-group">
             <p>Your Significant Other's Name</p>
             <input
+              onChange={e => this.onInputChange(e)}
               className="form-control"
               type="text"
-              name="lifeTogether[soName]"
+              name="soName"
               placeholder="Arwen"
-              required
             />
           </div>
           <div className="form-group">
             <p>Your Birthday</p>
             <input
+              onChange={e => this.onInputChange(e)}
               className="form-control"
               type="date"
-              name="lifeTogether[yourBday]"
-              required
+              name="yourBday"
             />
           </div>
           <div className="form-group">
             <p>Your Significant Other's Birthday</p>
             <input
+              onChange={e => this.onInputChange(e)}
               className="form-control"
               type="date"
-              name="lifeTogether[soBday]"
-              required
+              name="soBday"
             />
           </div>
           <div className="form-group">
             <p>Date Y'all Met</p>
             <input
+              onChange={e => this.onInputChange(e)}
               className="form-control"
               type="date"
-              name="lifeTogether[meetDate]"
-              required
+              name="meetDate"
             />
           </div>
           <div className="form-group">
             <p>Date Y'all Started Dating</p>
             <input
+              onChange={e => this.onInputChange(e)}
               className="form-control"
               type="date"
-              name="lifeTogether[datingDate]"
-              required
+              name="datingDate"
             />
           </div>
           <div className="form-group">
             <p>Date Y'all Were Married (Leave blank if not married)</p>
             <input
+              onChange={e => this.onInputChange(e)}
               className="form-control"
               type="date"
-              name="lifeTogether[marriedDate]"
+              name="marriedDate"
             />
           </div>
           <div className="form-group">
-            <button className="btn btn-lg btn-primary btn-block">Submit</button>
+            <button
+              onClick={this.handleDataChange}
+              className="btn btn-lg btn-primary btn-block"
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
