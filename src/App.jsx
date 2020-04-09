3c628d3c1279768a8d0e75466b4bd3feca64d9a9
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-09 06:07:32 -0400

    changed App and Display from rcc to rfc and is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,68 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import Display from "./components/Display.jsx";
 import Form from "./components/Form.jsx";
 import { Container } from "react-bootstrap";
-export default class App extends Component {
-  state = {
-    data: {
-      yourName: "Charles",
-      soName: "Catherine",
-      yourBday: "1992-12-18",
-      soBday: "1994-10-12",
-      meetDate: "2014-02-19",
-      datingDate: "2014-03-10",
-      marriedDate: "2018-01-06"
-    }
-  };
 
-  millisToDays = millis => {
+const App = () => {
+  const [yourName, setYourName] = useState("Charles");
+  const [soName, setSoName] = useState("Catherine");
+  const [yourBday, setYourBday] = useState("1992-12-18");
+  const [soBday, setSoBday] = useState("1994-10-12");
+  const [meetDate, setMeetDate] = useState("2014-02-19");
+  const [datingDate, setDatingDate] = useState("2014-03-10");
+  const [marriedDate, setMarriedDate] = useState("2014-03-10");
+
+  const millisToDays = (millis) => {
     var days = Math.floor(millis / 86400000);
     return days;
   };
 
-  handleUpdate = data => {
-    this.setState({ data: data });
+  useEffect(() => {
+    // setState({ data: data });
     window.scrollTo(0, 0);
-  };
-  render() {
-    let now = new Date();
+    return () => {};
+  }, []);
 
-    let age_user = this.millisToDays(now - new Date(this.state.data.yourBday));
-    let age_so = this.millisToDays(now - new Date(this.state.data.soBday));
-    let age_meet = this.millisToDays(now - new Date(this.state.data.meetDate));
-    let age_dating = this.millisToDays(
-      now - new Date(this.state.data.datingDate)
-    );
-    let age_married = this.millisToDays(
-      now - new Date(this.state.data.marriedDate)
-    );
-    let percent_user = ((age_meet / age_user) * 100).toFixed(2) + "%";
-    let percent_so = ((age_meet / age_so) * 100).toFixed(2) + "%";
+  let now = new Date();
 
-    return (
-      <div>
-        <Container className="view">
-          <h1 className="title">Life Together Calculator</h1>
-          <p>
-            Add some mathematical precision to your friendship || relationship
-            || marriage.
-          </p>
-          <Display
-            data={this.state.data}
-            age_meet={age_meet}
-            age_dating={age_dating}
-            age_married={age_married}
-            percent_user={percent_user}
-            percent_so={percent_so}
-          />
-          <h2>Try it yourself</h2>
-          <hr />
-          <Form onUpdate={this.handleUpdate} />
-          <footer className="footer">
-            <a href="https://cpustejovsky.com/" rel="noreferrer noopener">
-              Cpustejovsky <i className="far fa-thumbs-up"></i>,{" "}
-              {new Date().getFullYear()}
-            </a>
-          </footer>
-        </Container>
-      </div>
-    );
-  }
-}
+  let ageUser = millisToDays(now - new Date(yourBday));
+  let ageSo = millisToDays(now - new Date(soBday));
+  let ageMeet = millisToDays(now - new Date(meetDate));
+  let ageDating = millisToDays(now - new Date(datingDate));
+  let ageMarried = millisToDays(now - new Date(marriedDate));
+  let percentUser = ((ageMeet / ageUser) * 100).toFixed(2) + "%";
+  let percentSo = ((ageMeet / ageSo) * 100).toFixed(2) + "%";
+
+  return (
+    <div>
+      <Container className="view">
+        <h1 className="title">Life Together Calculator</h1>
+        <p>
+          Add some mathematical precision to your friendship || relationship ||
+          marriage.
+        </p>
+        <Display
+          yourName={yourName}
+          soName={soName}
+          ageMeet={ageMeet}
+          ageDating={ageDating}
+          ageMarried={ageMarried}
+          percentUser={percentUser}
+          percentSo={percentSo}
+        />
+        <h2>Try it yourself</h2>
+        <hr />
+        <Form />
+        <footer className="footer">
+          <a href="https://cpustejovsky.com/" rel="noreferrer noopener">
+            Cpustejovsky <i className="far fa-thumbs-up"></i>,{" "}
+            {new Date().getFullYear()}
+          </a>
+        </footer>
+      </Container>
+    </div>
+  );
+};
+
+export default App;
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,149 +1,148 @@
-import React, { Component } from "react";
+import React from "react";
 import { Col, Row } from "react-bootstrap";
 import "../App.scss";
 
-export default class Display extends Component {
-  render() {
-    const {
-      yourName,
-      soName,
-      yourBday,
-      soBday,
-      meetDate,
-      datingDate,
-      marriedDate
-    } = this.props.data;
-    const age_meet = this.props.age_meet;
-    const age_dating = this.props.age_dating;
-    const age_married = this.props.age_married;
-    const percent_user = this.props.percent_user;
-    const percent_so = this.props.percent_so;
+const Display = (props) => {
+  const {
+    yourName,
+    soName,
+    yourBday,
+    soBday,
+    meetDate,
+    datingDate,
+    marriedDate,
+  } = props;
+  const ageMeet = props.ageMeet;
+  const ageDating = props.ageDating;
+  const ageMarried = props.ageMarried;
+  const percentUser = props.percentUser;
+  const percentSo = props.percentSo;
 
-    const inputs = {
-      insufficient:
-        yourName === null ||
-        soName === null ||
-        yourBday === null ||
-        soBday === null ||
-        meetDate === null,
-      meeting:
-        yourName !== null &&
-        soName !== null &&
-        yourBday !== null &&
-        soBday !== null &&
-        meetDate !== null &&
-        datingDate === null &&
-        marriedDate === null,
-      dating:
-        yourName !== null &&
-        soName !== null &&
-        yourBday !== null &&
-        soBday !== null &&
-        meetDate !== null &&
-        datingDate !== null &&
-        marriedDate === null,
-      married:
-        yourName !== null &&
-        soName !== null &&
-        yourBday !== null &&
-        soBday !== null &&
-        meetDate !== null &&
-        datingDate !== null &&
-        marriedDate !== null
-    };
+  const inputs = {
+    insufficient:
+      yourName === null ||
+      soName === null ||
+      yourBday === null ||
+      soBday === null ||
+      meetDate === null,
+    meeting:
+      yourName !== null &&
+      soName !== null &&
+      yourBday !== null &&
+      soBday !== null &&
+      meetDate !== null &&
+      datingDate === null &&
+      marriedDate === null,
+    dating:
+      yourName !== null &&
+      soName !== null &&
+      yourBday !== null &&
+      soBday !== null &&
+      meetDate !== null &&
+      datingDate !== null &&
+      marriedDate === null,
+    married:
+      yourName !== null &&
+      soName !== null &&
+      yourBday !== null &&
+      soBday !== null &&
+      meetDate !== null &&
+      datingDate !== null &&
+      marriedDate !== null,
+  };
 
-    if (inputs.insufficient) {
-      return (
-        <h3 class="error">
-          Please provide at least <strong>two names</strong>,{" "}
-          <strong>two birthdays</strong>, and{" "}
-          <strong>the date y'all met</strong>.
-        </h3>
-      );
-    }
-    if (inputs.meeting) {
-      return (
-        <Row className="display">
-          <Col md={5} className="display__box">
-            {" "}
-            <p>
-              {yourName} has known {soName} <strong>{percent_user}</strong> of
-              their lives.
-            </p>
-          </Col>
-          <Col md={5} className="display__box">
-            {" "}
-            <p>
-              {soName} has known {yourName} <strong>{percent_so}</strong> of
-              their lives.
-            </p>
-          </Col>
-          <Col md={6} className="display__box">
-            <p>
-              They met <strong>{age_meet}</strong> days ago.
-            </p>
-          </Col>
-        </Row>
-      );
-    }
-    if (inputs.dating) {
-      return (
-        <Row className="display">
-          <Col md={5} className="display__box">
-            {" "}
-            <p>
-              {yourName} has known {soName} <strong>{percent_user}</strong> of
-              their lives.
-            </p>
-          </Col>
-          <Col md={5} className="display__box">
-            {" "}
-            <p>
-              {soName} has known {yourName} <strong>{percent_so}</strong> of
-              their lives.
-            </p>
-          </Col>
-          <Col md={6} className="display__box">
-            <p>
-              They met <strong>{age_meet}</strong> days ago.
-            </p>
-            <p>
-              They started dating <strong>{age_dating}</strong> days ago.
-            </p>
-          </Col>
-        </Row>
-      );
-    }
-    if (inputs.married) {
-      return (
-        <Row className="display">
-          <Col md={5} className="display__box">
-            {" "}
-            <p>
-              {yourName} has known {soName} <strong>{percent_user}</strong> of
-              their lives.
-            </p>
-          </Col>
-          <Col md={5} className="display__box">
-            {" "}
-            <p>
-              {soName} has known {yourName} <strong>{percent_so}</strong> of
-              their lives.
-            </p>
-          </Col>
-          <Col md={6} className="display__box">
-            <p>
-              They met <strong>{age_meet}</strong> days ago.
-            </p>
-            <p>
-              They started dating <strong>{age_dating}</strong> days ago.
-            </p>
-            <p>
-              They were married <strong>{age_married}</strong> days ago.
-            </p>
-          </Col>
-        </Row>
-      );
-    }
+  if (inputs.insufficient) {
+    return (
+      <h3 class="error">
+        Please provide at least <strong>two names</strong>,{" "}
+        <strong>two birthdays</strong>, and <strong>the date y'all met</strong>.
+      </h3>
+    );
   }
-}
+  if (inputs.meeting) {
+    return (
+      <Row className="display">
+        <Col md={5} className="display__box">
+          {" "}
+          <p>
+            {yourName} has known {soName} <strong>{percentUser}</strong> of
+            their lives.
+          </p>
+        </Col>
+        <Col md={5} className="display__box">
+          {" "}
+          <p>
+            {soName} has known {yourName} <strong>{percentSo}</strong> of their
+            lives.
+          </p>
+        </Col>
+        <Col md={6} className="display__box">
+          <p>
+            They met <strong>{ageMeet}</strong> days ago.
+          </p>
+        </Col>
+      </Row>
+    );
+  }
+  if (inputs.dating) {
+    return (
+      <Row className="display">
+        <Col md={5} className="display__box">
+          {" "}
+          <p>
+            {yourName} has known {soName} <strong>{percentUser}</strong> of
+            their lives.
+          </p>
+        </Col>
+        <Col md={5} className="display__box">
+          {" "}
+          <p>
+            {soName} has known {yourName} <strong>{percentSo}</strong> of their
+            lives.
+          </p>
+        </Col>
+        <Col md={6} className="display__box">
+          <p>
+            They met <strong>{ageMeet}</strong> days ago.
+          </p>
+          <p>
+            They started dating <strong>{ageDating}</strong> days ago.
+          </p>
+        </Col>
+      </Row>
+    );
+  }
+  if (inputs.married) {
+    return (
+      <Row className="display">
+        <Col md={5} className="display__box">
+          {" "}
+          <p>
+            {yourName} has known {soName} <strong>{percentUser}</strong> of
+            their lives.
+          </p>
+        </Col>
+        <Col md={5} className="display__box">
+          {" "}
+          <p>
+            {soName} has known {yourName} <strong>{percentSo}</strong> of their
+            lives.
+          </p>
+        </Col>
+        <Col md={6} className="display__box">
+          <p>
+            They met <strong>{ageMeet}</strong> days ago.
+          </p>
+          <p>
+            They started dating <strong>{ageDating}</strong> days ago.
+          </p>
+          <p>
+            They were married <strong>{ageMarried}</strong> days ago.
+          </p>
+        </Col>
+      </Row>
+    );
+  }
+};
+
+export default Display;
