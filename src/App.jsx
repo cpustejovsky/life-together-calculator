b165159d599e8d44f0c99dd7e7bdd8f61c353b7f
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-09 06:32:33 -0400

    clean up; changing unmutated `let`s to `const`s

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import Display from "./components/Display.jsx";
 import Form from "./components/Form.jsx";
@@ -11,33 +11,29 @@ const App = () => {
   const [soBday, setSoBday] = useState("1994-10-12");
   const [meetDate, setMeetDate] = useState("2014-02-19");
   const [datingDate, setDatingDate] = useState("2014-03-10");
-  const [marriedDate, setMarriedDate] = useState("2014-03-10");
+  const [marriedDate, setMarriedDate] = useState("2018-01-06");
 
-  const millisToDays = (millis) => {
-    var days = Math.floor(millis / 86400000);
-    return days;
-  };
+  const millisToDays = (millis) => Math.floor(millis / 86400000);
 
   const onUpdate = (data) => {
-    setYourName(data.yourName)
-    setSoName(data.soName)
-    setYourBday(data.yourBday)
-    setSoBday(data.soBday)
-    setMeetDate(data.meetDate)
-    setDatingDate(data.datingDate)
-    setMarriedDate(data.marriedDate)
+    setYourName(data.yourName);
+    setSoName(data.soName);
+    setYourBday(data.yourBday);
+    setSoBday(data.soBday);
+    setMeetDate(data.meetDate);
+    setDatingDate(data.datingDate);
+    setMarriedDate(data.marriedDate);
+  };
 
-  }
+  const now = new Date();
 
-  let now = new Date();
-
-  let ageUser = millisToDays(now - new Date(yourBday));
-  let ageSo = millisToDays(now - new Date(soBday));
-  let ageMeet = millisToDays(now - new Date(meetDate));
-  let ageDating = millisToDays(now - new Date(datingDate));
-  let ageMarried = millisToDays(now - new Date(marriedDate));
-  let percentUser = ((ageMeet / ageUser) * 100).toFixed(2) + "%";
-  let percentSo = ((ageMeet / ageSo) * 100).toFixed(2) + "%";
+  const ageUser = millisToDays(now - new Date(yourBday));
+  const ageSo = millisToDays(now - new Date(soBday));
+  const ageMeet = millisToDays(now - new Date(meetDate));
+  const ageDating = millisToDays(now - new Date(datingDate));
+  const ageMarried = millisToDays(now - new Date(marriedDate));
+  const percentUser = ((ageMeet / ageUser) * 100).toFixed(2) + "%";
+  const percentSo = ((ageMeet / ageSo) * 100).toFixed(2) + "%";
 
   return (
     <div>
@@ -58,7 +54,7 @@ const App = () => {
         />
         <h2>Try it yourself</h2>
         <hr />
-        <Form onUpdate={onUpdate}/>
+        <Form onUpdate={onUpdate} />
         <footer className="footer">
           <a href="https://cpustejovsky.com/" rel="noreferrer noopener">
             Cpustejovsky <i className="far fa-thumbs-up"></i>,{" "}
