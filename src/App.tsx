7661cb222438cc96c33e77c3f9f864d0df5cb186
cpustejovsky <charles.pustejovsky@gmail.com>
2020-10-02 20:34:37 -0400

    getting typescript working for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,38 @@ const App = () => {
   const [yourBday, setYourBday] = useState("1992-12-18");
   const [soBday, setSoBday] = useState("1994-10-12");
   const [meetDate, setMeetDate] = useState("2014-02-19");
-  const [datingDate, setDatingDate] = useState("2014-03-10");
-  const [marriedDate, setMarriedDate] = useState("2018-01-06");
+  const [datingDate, setDatingDate] = useState<string>("2014-03-10");
+  const [marriedDate, setMarriedDate] = useState<string>("2018-01-06");
 
-  const millisToDays = (millis) => Math.floor(millis / 86400000);
+  const millisToDays = (millis: number) => Math.floor(millis / 86400000);
 
-  const onUpdate = (data) => {
+  type DateData = {
+    yourName: string,
+    soName: string,
+    yourBday: string,
+    soBday: string,
+    meetDate: string,
+    datingDate?: string,
+    marriedDate?: string,
+  }
+
+  const onUpdate = (data: DateData) => {
     setYourName(data.yourName);
     setSoName(data.soName);
     setYourBday(data.yourBday);
     setSoBday(data.soBday);
     setMeetDate(data.meetDate);
-    setDatingDate(data.datingDate);
-    setMarriedDate(data.marriedDate);
+    setDatingDate(data.datingDate ? data.datingDate : "");
+    setMarriedDate(data.marriedDate ? data.marriedDate : "");
   };
 
-  const now = new Date();
+  const now = new Date().getTime();
 
-  const ageUser = millisToDays(now - new Date(yourBday));
-  const ageSo = millisToDays(now - new Date(soBday));
-  const ageMeet = millisToDays(now - new Date(meetDate));
-  const ageDating = millisToDays(now - new Date(datingDate));
-  const ageMarried = millisToDays(now - new Date(marriedDate));
+  const ageUser = millisToDays(now - new Date(yourBday).getTime());
+  const ageSo = millisToDays(now - new Date(soBday).getTime());
+  const ageMeet = millisToDays(now - new Date(meetDate).getTime());
+  const ageDating = millisToDays(now - new Date(datingDate).getTime());
+  const ageMarried = millisToDays(now - new Date(marriedDate).getTime());
   const percentUser = ((ageMeet / ageUser) * 100).toFixed(2) + "%";
   const percentSo = ((ageMeet / ageSo) * 100).toFixed(2) + "%";
 
